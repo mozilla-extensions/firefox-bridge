99424d79be129d69c1a1492329f85db256fad7b2
Greg Sherman <gsherman@mozilla.com>
2024-01-30 16:14:46 -0500

    add await to async launchbrowser function

--- src/firefox/interfaces/listeners.js
+++ src/firefox/interfaces/listeners.js
@@ -6,7 +6,7 @@ import { getExternalBrowser } from "../../shared/backgroundScripts/getters.js";
  */
 export function initPlatformListeners() {
   browser.browserAction.onClicked.addListener(async (tab) => {
-    if (launchBrowser(tab)) {
+    if (await launchBrowser(tab)) {
       browser.storage.local.set({
         telemetry: {
           type: "browserLaunch",
