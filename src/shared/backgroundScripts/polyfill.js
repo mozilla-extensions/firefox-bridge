5a469e229a7f442231c4f78c62e41a405f678096
Greg Sherman <gsherman@mozilla.com>
2024-01-31 11:17:10 -0500

    remove unnecessary globalThis prefixes

--- src/shared/backgroundScripts/polyfill.js
+++ src/shared/backgroundScripts/polyfill.js
@@ -2,6 +2,6 @@
  * Polyfill the browser object to be compatible with both Chrome MV3 and Firefox MV2.
  */
 export function polyfillBrowser() {
-  globalThis.browser ??= globalThis.chrome;
-  globalThis.browser.action ??= globalThis.browser.browserAction;
+  globalThis.browser ??= chrome;
+  browser.action ??= browser.browserAction;
 }
