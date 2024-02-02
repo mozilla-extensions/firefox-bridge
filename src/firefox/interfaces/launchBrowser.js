f02e849fcab6060320e6d66f86c73b3b02330b15
Greg Sherman <gsherman@mozilla.com>
2024-02-02 14:32:58 -0500

    log an error for launching chromium in private mode

--- src/firefox/interfaces/launchBrowser.js
+++ src/firefox/interfaces/launchBrowser.js
@@ -5,9 +5,18 @@ import { getExternalBrowserLaunchProtocol } from "./getters.js";
  * Launches the user set browser. If the browser is not set, launch the welcome page.
  *
  * @param {*} url The url to launch in the browser.
+ * @param {boolean} usePrivateBrowsing True if the browser should be launched in private browsing mode, false for normal mode.
  * @returns {boolean} True if the browser was launched, false otherwise.
  */
-export async function launchBrowser(url) {
+export async function launchBrowser(url, usePrivateBrowsing = false) {
+  if (usePrivateBrowsing) {
+    // NOTE: an argument of --incognito in the launchApp call would launch the browser in private mode in chromium.
+    console.error(
+      "Private browsing launching is not yet supported from Firefox.",
+    );
+    return false;
+  }
+
   if (isCurrentTabValidUrlScheme) {
     const launchProtocol = await getExternalBrowserLaunchProtocol();
     if (launchProtocol) {
